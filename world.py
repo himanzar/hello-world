84cf0fc0dd492865ff77fe3f32b3536f547d8bee
amirali manzar <91746714+himanzar@users.noreply.github.com>
2021-10-02 09:02:52 -0700

    Create world.py

---
+++ world.py
@@ -0,0 +1,13 @@
+name = input ("what is your name? ")
+print ("hello, " + name)
+print (name + " welcome to Python")
+res = input(name + ", are you a programmer?" + "[Yes / No]")
+
+if res == 'Yes':
+  print(name + ", Welcome to the world of programming!")
+if res == 'No':
+  print("Oh, this app is not for you.")
+
+else:
+  print("Oops, we did not expect such an answer!")
+# Coded by Mr.Manzar
