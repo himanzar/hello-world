10bf473f5cfb76a3b82b4f1323a5527c279ec65c
amirali manzar <91746714+himanzar@users.noreply.github.com>
2021-10-02 09:02:36 -0700

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,16 @@
+
+name = input("what is your name? ")
+print (f""" \n \n# hello, {name} 
+# welcom to python! \n""")
+res = input("Do you know Python? ")
+while True:
+ if res.lower()=="yes":
+  print("it's ok")
+  break
+ elif res.lower()=="no":
+  print("you can't use!")
+  break
+ else:
+  print("you can enter 'yse' or 'no only!!")
+  res=input("Do you know Python?")
+  # Coded by Mr.Manzar
